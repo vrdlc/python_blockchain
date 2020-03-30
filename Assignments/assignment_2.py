c2cd349ed6ab6c3b81f1c6975a4401a3a2869620
vrdlc <virgilioramon.delacruz@gmail.com>
2020-03-30 08:36:04 -0700

    assignment 2 complete, commented out code is a messy way to get the same results using nested for loops and a while loop

--- Assignments/assignment_2.py
+++ Assignments/assignment_2.py
@@ -1,25 +1,57 @@
 # TODO:
-# 1. Create a list of names and use a for loop to output the length of each name
-# 2. Add an if check inside the loop to only output names longer than 5 characters
+# 1. Create a list of names and use a for loop to output the length of each name DONE
+# 2. Add an if check inside the loop to only output names longer than 5 characters DONE
 # 3. Add another if check to see whether a name includes an "n" or "N" characters
 # 4. Use a while loop to empty the list of names (via pop())
 
 
-name_list = ['Jim', 'Jan', 'Claire', 'Claire', 'Manacle', 'Art', 'Nana', 'Bon']
+names = ['Jim', 'Jan', 'Claire', 'Manacle', 'Art', 'Nala', 'Bon', 'Jason', 'Nannette']
 five_char = []
+n_char = []
 
 def name_len():
-    # 'name' is the index point
-    for name in range(len(name_list)):
-        # name_list[name] is the name in the array at that index point
-        if len(name_list[name]) >= 5:
-            # This adds the a name > 5 characters to the array
-            five_char.append(name_list[name])
-        else:
-            continue
+    i = 0
 
-    # Remember to remove an indent to remove something from a loop but to keep it in the function. Python has no dedicated closing characters!
+# This is the elegant solution, but I wanted my nested loops to work more effectively. I can get the required resultes, but it's not clean.
+    for name in names:
+        if len(name) >= 5 and 'n' in name:
+            five_char.append(name)
     print(five_char)
 
+    while five_char:
+        print(five_char.pop())
+
+    print(five_char)
+#I was trying to use a for loop to pop out names when I needed a while loop...
+    # for name in names:
+    #     # print(name)
+    #     # name_list[name] is the name in the array at that index point
+    #     if len(name) >= 5:
+    #         # This adds the a name > 5 characters to the array
+    #         five_char.append(name)
+    #         # print(five_char)
+    #
+    #         # I'm sure there's a match() style function, but I want to play with for loops
+    #         for name in five_char:
+    #             # print(name)
+    #             # i = i + 1
+    #             for char in name:
+    #                 if char == 'n':
+    #                     n_char.append(name)
+    #                 else:
+    #                     if len(five_char) <= 1:
+    #                         break
+    #
+    #                 if five_char[char] == 'n':
+    #                     n_char.append(name)
+    #                     print(n_char)
+    #             while five_char:
+    #                 print(five_char.pop())
+
+
+
+    # Remember to remove an indent to remove something from a loop but to keep it in the function. Python has no dedicated closing characters!
+        # print(n_char)
+        # print(five_char)
 
 name_len()
